c31be00b240fe5483c63ac7d4073c6c87b9946e0
farahnaz <farahnaz9090shahmoradi@gmail.com>
2023-10-01 09:26:08 +0330

    end

--- backend.py
+++ backend.py
@@ -3,15 +3,15 @@ import requests
 API_Key = "0660e01b8a5c38ff695c856bf12b36c1"
 
 
-def get_data(place,forecast_days=None):
+def get_data(place, forecast_days=None):
     url=f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_Key}"
     response = requests.get(url)
     data = response.json()
     filterd_data = data["list"]
-    nr_values = 8*forecast_days
+    nr_values = 8 * forecast_days
     filterd_data = filterd_data[:nr_values]
     return filterd_data
 
 
-if __name__ =="__main__":
-    print(get_data(place="Tokyo",forecast_days=3))
+if __name__ == "__main__":
+    print(get_data(place="Tokyo", forecast_days=3))
--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from backend import get_data
 st.title("Weather forecast for the next Days")
 place = st.text_input("place: ")
 
-days = st.slider("Forecast Days",min_value=1,max_value=5,
+days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="select number of forecast days")
 
-option = st.selectbox("select Data to view",("Temperature","Sky"))
+option = st.selectbox("select Data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
 if place:
     try:
-        filterd_data = get_data(place,days)
+        filterd_data = get_data(place, days)
 
         if option == "Temperature":
             temperatures = [dict["main"]["temp"] for dict in filterd_data]
@@ -21,15 +21,15 @@ if place:
             temperatures = [tempreture/10 for tempreture in temperatures ]
 
             dates = [dict["dt_txt"] for dict in filterd_data]
-            figure = px.line(x=dates,y=temperatures,labels={"x":"Date","y":"temperature (c)"})
+            figure = px.line(x=dates, y=temperatures, labels={"x":"Date","y":"temperature (c)"})
             st.plotly_chart(figure)
 
         if option == "Sky":
-            image = {"Clear":"images/clear.png","Clouds":"images/cloud.png"
-                     ,"Rain":"images/rain.png","Snow":"images/snow.png"}
+            image = {"Clear": "images/clear.png", "Clouds": "images/cloud.png"
+                     , "Rain": "images/rain.png", "Snow": "images/snow.png"}
             sky_conditions = [dict["weather"][0]["main"] for dict in filterd_data]
-            image_path=[image[condition] for condition in sky_conditions]
-            st.image(image_path,width=115)
+            image_path = [image[condition] for condition in sky_conditions]
+            st.image(image_path, width=115)
 
     except KeyError:
         st.write("There is No city with this name!!")
